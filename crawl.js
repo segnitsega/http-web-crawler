6b3826f9c5b5eb0c6cd0e8156a7d91e03f30ef4a
segnitsega <segnitsega6@gmail.com>
2025-11-30 11:57:39 +0300

    getUrlFromHtml and tests

--- crawl.js
+++ crawl.js
@@ -1,12 +1,40 @@
-function normalizeUrl(url) {
-    const urlObj = new URL(url);
-    url = `${urlObj.hostname}${urlObj.pathname}`;
-    if(url.endsWith("/")) {
-        url = url.slice(0, -1);
+const { JSDOM } = require("jsdom");
+
+function getUrlFromHtml(htmlBody, baseUrl) {
+  // const {JSDOM} = await import("jsdom");
+  const dom = new JSDOM(htmlBody);
+  const links = dom.window.document.querySelectorAll("a");
+  const urls = [];
+  links.forEach((link) => {
+    if (link.href.startsWith("/")) {
+      urls.push(baseUrl + link.href);
     }
-    return url
+    else {
+      try {
+        const urlObj = new URL(link.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log("Error ", err.message);
+      }
+    }
+  });
+  console.log(urls);
+  return urls;
+}
+
+const inputHtmlBody = `<html><body><a href="/path">Link</a><a href="invalid">Link</a><<a href="https://google.com">Link</a>/body></html>`;
+getUrlFromHtml(inputHtmlBody, "http://google.com");
+
+function normalizeUrl(url) {
+  const urlObj = new URL(url);
+  url = `${urlObj.hostname}${urlObj.pathname}`;
+  if (url.endsWith("/")) {
+    url = url.slice(0, -1);
+  }
+  return url;
 }
 
 module.exports = {
-    normalizeUrl
-}
+  normalizeUrl,
+  getUrlFromHtml,
+};
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeUrl } = require("./crawl");
+const { normalizeUrl, getUrlFromHtml } = require("./crawl");
 
 test("normalizeUrl strip protocols", () => {
   const inputUrl = "http://example.com/path";
@@ -20,3 +20,38 @@ test("normalizeUrl capitals", () => {
   const expectedUrl = "example.com/path";
   expect(normalizedUrl).toBe(expectedUrl);
 });
+
+test("get asbsolute urls from html", () => {
+  const inputHtmlBody = `<html><body><a href="http://example.com/path/">Link</a><a href="http://segni.com">Link</a></body></html>`;
+  const inputBaseUrl = "http://example.com";
+  const extractedUrls = getUrlFromHtml(inputHtmlBody, inputBaseUrl);
+  const expectedUrls = ["http://example.com/path/", "http://segni.com/"];
+  expect(extractedUrls).toEqual(expectedUrls);
+});
+
+test("get relative urls from html", () => {
+  const inputHtmlBody = `<html><body><a href="/path">Link</a></body></html>`;
+  const inputBaseUrl = "http://example.com";
+  const extractedUrls = getUrlFromHtml(inputHtmlBody, inputBaseUrl);
+  const expectedUrls = ["http://example.com/path"];
+  expect(extractedUrls).toEqual(expectedUrls);
+});
+
+test("get asbsolute, and relative urls from html", () => {
+  const inputHtmlBody = `<html><body><a href="http://example.com/path/">Link</a><a href="/path2">Link</a></body></html>`;
+
+  const inputBaseUrl = "http://example.com";
+  const extractedUrls = getUrlFromHtml(inputHtmlBody, inputBaseUrl);
+  const expectedUrls = ["http://example.com/path/", "http://example.com/path2"];
+  expect(extractedUrls).toEqual(expectedUrls);
+});
+
+test("don't include invalid urls from html", () => {
+  const inputHtmlBody = `<html><body><a href="invalid">invalid url</a></body></html>`;
+
+  const inputBaseUrl = "http://example.com";
+  const extractedUrls = getUrlFromHtml(inputHtmlBody, inputBaseUrl);
+  const expectedUrls = [];
+  expect(extractedUrls).toEqual(expectedUrls);
+});
+
